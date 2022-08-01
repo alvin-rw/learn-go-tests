b5018abe4143d914450c7b089a2a2a41e2979d1a
alvin-rw <alvin.rw@proton.me>
2022-08-01 15:18:51 +0800

    refactor: implement area as method of each shape, add circle shape

--- 5-structs-methods-interfaces/perimeter.go
+++ 5-structs-methods-interfaces/perimeter.go
@@ -1,14 +1,24 @@
 package rectangle
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(r Rectangle) float64 {
-	return 2 * (r.Width + r.Height)
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
-func Area(r Rectangle) float64 {
-	return r.Width * r.Height
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
 }
