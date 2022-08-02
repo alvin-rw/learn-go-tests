707e82174bbd6b2bc49f0869154f869eaa9c04a4
alvin-rw <alvin.rw@proton.me>
2022-08-02 14:57:16 +0800

    feat: add configurable sleeper

--- 9-mocking/countdown.go
+++ 9-mocking/countdown.go
@@ -11,6 +11,23 @@ type Sleeper interface {
 	Sleep()
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
@@ -49,6 +66,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
