782671bfe9a6210cc0af3674185839bdd388937a
alvin-rw <alvin.rw@proton.me>
2022-08-02 16:10:55 +0800

    feat: initial concurrency test

---
+++ 10-concurrency/concurrency_test.go
@@ -0,0 +1,32 @@
+package concurrency
+
+import (
+	"reflect"
+	"testing"
+)
+
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furtherwe.geds" {
+		return false
+	}
+	return true
+}
+
+func TestCheckWebsite(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furtherwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furtherwe.geds":      false,
+	}
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
