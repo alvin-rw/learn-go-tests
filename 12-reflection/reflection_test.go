c914cd0b5ce0a792b12541da7611d8ac7f3a71af
Alvin Wonys <alvin.rw@proton.me>
2022-08-13 12:22:39 +0800

    refactor: refactor test case for maps

--- 12-reflection/reflection_test.go
+++ 12-reflection/reflection_test.go
@@ -16,86 +16,109 @@ type Profile struct {
 }
 
 func TestWalk(t *testing.T) {
-	cases := []struct {
-		Name          string
-		Input         interface{}
-		ExpectedCalls []string
-	}{
-		{
-			"struct with one string field",
-			struct {
-				Name string
-			}{"Chris"},
-			[]string{"Chris"},
-		},
-		{
-			"struct with two string fields",
-			struct {
-				Name string
-				City string
-			}{"Chris", "London"},
-			[]string{"Chris", "London"},
-		},
-		{
-			"struct with non string field",
-			struct {
-				Name string
-				Age  int
-			}{"Chris", 33},
-			[]string{"Chris"},
-		},
-		{
-			"nested fields",
-			Person{
-				"Chris",
-				Profile{33, "London"},
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("other types", func(t *testing.T) {
+		cases := []struct {
+			Name          string
+			Input         interface{}
+			ExpectedCalls []string
+		}{
+			{
+				"struct with one string field",
+				struct {
+					Name string
+				}{"Chris"},
+				[]string{"Chris"},
 			},
-			[]string{"Chris", "London"},
-		},
-		{
-			"pointers to things",
-			&Person{
-				"Chris",
-				Profile{33, "London"},
+			{
+				"struct with two string fields",
+				struct {
+					Name string
+					City string
+				}{"Chris", "London"},
+				[]string{"Chris", "London"},
 			},
-			[]string{"Chris", "London"},
-		},
-		{
-			"slices",
-			[]Profile{
-				{33, "London"},
-				{34, "Reykjavík"},
+			{
+				"struct with non string field",
+				struct {
+					Name string
+					Age  int
+				}{"Chris", 33},
+				[]string{"Chris"},
 			},
-			[]string{"London", "Reykjavík"},
-		},
-		{
-			"arrays",
-			[2]Profile{
-				{33, "London"},
-				{34, "Reykjavík"},
+			{
+				"nested fields",
+				Person{
+					"Chris",
+					Profile{33, "London"},
+				},
+				[]string{"Chris", "London"},
 			},
-			[]string{"London", "Reykjavík"},
-		},
-		{
-			"maps",
-			map[string]string{
-				"Foo": "Bar",
-				"Baz": "Boz",
+			{
+				"pointers to things",
+				&Person{
+					"Chris",
+					Profile{33, "London"},
+				},
+				[]string{"Chris", "London"},
 			},
-			[]string{"Bar", "Boz"},
-		},
+			{
+				"slices",
+				[]Profile{
+					{33, "London"},
+					{34, "Reykjavík"},
+				},
+				[]string{"London", "Reykjavík"},
+			},
+			{
+				"arrays",
+				[2]Profile{
+					{33, "London"},
+					{34, "Reykjavík"},
+				},
+				[]string{"London", "Reykjavík"},
+			},
+		}
+
+		for _, test := range cases {
+			t.Run(test.Name, func(t *testing.T) {
+				var got []string
+				walk(test.Input, func(input string) {
+					got = append(got, input)
+				})
+
+				if !reflect.DeepEqual(got, test.ExpectedCalls) {
+					t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+				}
+			})
+		}
+	})
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
 	}
 
-	for _, test := range cases {
-		t.Run(test.Name, func(t *testing.T) {
-			var got []string
-			walk(test.Input, func(input string) {
-				got = append(got, input)
-			})
-
-			if !reflect.DeepEqual(got, test.ExpectedCalls) {
-				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
-			}
-		})
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
 	}
 }
